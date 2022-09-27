259abfccf940ba6e128d722cb4010dc1d29a3ac4
Gaurav <gauravsomani52750@gmail.com>
2022-09-27 20:00:09 +0530

    Assignment 05 done

--- Workbench/Procedures.sql
+++ Workbench/Procedures.sql
@@ -138,6 +138,150 @@
 -- call cal_fine(1, 'The Godfather');
 
 
+-- Assignment no 05:
+-- Stud_Marks(Roll, name, total_marks)
+-- Result(Roll, name, class)
+
+create table Stud_Marks(
+	Roll_no int primary key,
+    name varchar(20),
+    total_marks int
+);
+
+insert into Stud_Marks values (1, "Gaurav", 1400);
+insert into Stud_Marks values (2, "Rahul", 850);
+insert into Stud_Marks values (3, "Rohit", 980);
+
+create table Result(
+	Roll_no int,
+    name varchar(20),
+    class varchar(20),
+    foreign key (Roll_no) references Stud_Marks(Roll_no)
+);
+
+-- desc Result;
+
+drop procedure if exists proc_Grade;
+
+-- delimiter $$
+-- create procedure proc_Grade(
+-- 	Roll_no int
+-- )
+-- begin
+-- 	declare marks int;
+--     declare stud_name varchar(20);
+--     declare class varchar(20);
+--     
+--     -- setting the marks:
+--     select total_marks, name
+--     from Stud_Marks s
+--     where s.Roll_no = Roll_no
+--     INTO marks, stud_name;
+--     
+--     IF marks < 825 THEN
+-- 		SIGNAL SQLSTATE '22003'
+-- 			SET message_text = 'Marks must be greater than 824!';
+--     END IF;
+--     
+--     
+--     IF marks between 990 and 1500 THEN
+-- 		set class = 'Distinction';
+-- 	ELSEIF marks between 900 and 989 THEN
+-- 		set class = 'First class';
+-- 	ELSEIF marks between 825 and 899 THEN
+-- 		set class = 'Higher second class';
+-- 	END IF;
+--     
+--     -- insert the values into Result table:
+--     insert into Result values(
+-- 		Roll_no,
+--         stud_name,
+--         class
+--     );
+--    
+-- end$$
+-- delimiter ;
+
+-- call proc_Grade(1);
+-- call proc_Grade(2);
+-- call proc_Grade(3);
+
+-- insert into Stud_Marks values(
+-- 	4, "Rishabh", 820
+-- );
+-- call proc_Grade(4);
+
+
+-- FUNCTION:
+drop function if exists calClass;
+
+delimiter $$
+create function calClass(
+	Roll_no int
+)
+returns varchar(20)
+deterministic -- attribute
+begin
+	declare marks int;
+	declare class varchar(20);
+    
+    select total_marks
+    from Stud_Marks s
+    where s.Roll_no = Roll_no
+    INTO marks;
+    
+    IF marks < 825 THEN
+		SIGNAL SQLSTATE '22003'
+			SET message_text = 'Marks must be greater than 824!';
+    END IF;
+
+    
+    IF marks between 990 and 1500 THEN
+		set class = 'Distinction';
+	ELSEIF marks between 900 and 989 THEN
+		set class = 'First class';
+	ELSEIF marks between 825 and 899 THEN
+		set class = 'Higher second class';
+	END IF;
+    
+	return class;
+end$$
+delimiter ;
+
+drop table if exists Result;
+
+create table Result as
+	select 
+		Roll_no,
+		name,
+		calClass(Roll_no) as Class
+	from Stud_Marks
+	where Roll_no != 4;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
